fdcff0dd770c4c1f77c97d27b77f91d8ae0a8be7
bbriceno <42550883+bbriceno@users.noreply.github.com>
2019-04-23 21:08:13 -0500

    Create lab2-p1-letterLib.h

---
+++ lab2-p1-letterLib.h
@@ -0,0 +1,57 @@
+# csc250-lab2
+void drawCharJ(double,double,double);
+
+
+void drawCharC(double upperleftx, double upperlefty, double height);
+
+
+void drawCharD(double ulCornerX, double ulCornerY, double height);
+
+
+void drawCharE(double ulCornerX, double ulCornerY, double height );
+
+
+void drawCharH(double ulCornerX, double ulCornerY, double height);
+
+
+void drawCharI( double ulCornerX, double ulCornerY, double height);
+
+
+void drawCharL(double ulCornerX, double ulCornerY, double height);
+
+
+void drawCharN(double ulCornerX, double ulCornerY, double height);
+
+
+void drawCharO(double ulCornerX, double ulCornerY, double radius);
+
+
+void drawCharQ(double ulCornerX, double ulCornerY, double height);
+
+
+void drawCharR(double ulCornerX, double ulCornerY, double height);
+
+
+void drawCharS(double ulCornerX, double ulCornerY, double height);
+
+
+void drawCharT(double ulCornerX,double ulCornerY, double height);
+
+
+void drawCharU (double ulCornerX, double ulCornerY, double height);
+
+
+void drawCharV(double ulCornerX, double ulCornerY, double height);
+
+
+void drawCharY(double ulCornerX, double ulCornerY, double side);
+
+
+void drawCharZ(double ulCornerX, double ulCornerY, double height);
+
+
+void drawMissingLetter(double ulCornerX, double ulCornerY, double height);
+
+void drawText(char* text, double x, double y, double height);
+
+
